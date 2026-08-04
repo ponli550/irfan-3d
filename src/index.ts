f167983ed3f5fc00dcec91eac47667e064a56b9c
Irfan Ali <nazrijz336@gmail.com>
2026-08-04 20:59:50 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ export class App extends DurableObject {
     return new Response("Not found", { status: 404 });
   }
 }
+
+export default {
+  async fetch(request: Request, env: { ASSETS: { fetch: (req: Request) => Promise<Response> } }): Promise<Response> {
+    return env.ASSETS.fetch(request);
+  },
+};
